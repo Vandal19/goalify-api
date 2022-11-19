af7c735ff62bf0fbc84b651684fc51694ff923f7
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-11-18 17:11:16 -0800

    Implement asyncHandler instead of using try/catch for async

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -1,36 +1,36 @@
+const asyncHandler = require('express-async-handler')
 
-
-const getGoals = async (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   try {
     await res.status(200).json({ message: "Get goals" });
   } catch (error) {
     console.error(error.response ? error.response.body : error);
   }
-};
+});
 
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async(req, res) => {
   if (!req.body.text) {
     res.status(400)
     throw new Error('Please add a text field')
   }
      res.status(200).json({ message: "Set Goal" });
-};
+});
 
-const updateGoal = async (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   try {
     await res.status(200).json({ message: `Update goals ${req.params.id}` });
   } catch (error) {
     console.error(error.response ? error.response.body : error);
   }
-};
+});
 
-const deleteGoal = async (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   try {
     await res.status(200).json({ message: `Delete goals ${req.params.id}` });
   } catch (error) {
     console.error(error.response ? error.response.body : error);
   }
-};
+});
 
 module.exports = {
   getGoals,
