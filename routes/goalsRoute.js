5c8fcdfbfb7f1dc8c3761a1c0dc30212b87bb7be
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-11-18 16:26:22 -0800

    Set up POST, PUT and DELETE request

--- routes/goalsRoute.js
+++ routes/goalsRoute.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/goals', async (req,res) => {
+router.get("/goals", async (req, res) => {
   try {
-    res.status(200).json({message: 'Get goals'})
+    res.status(200).json({ message: "Get goals" });
   } catch (error) {
     console.error(error.response ? error.response.body : error);
   }
-})
+});
+
+router.post("/goals", async (req, res) => {
+  try {
+    res.status(200).json({ message: "Set Goal" });
+  } catch (error) {
+    console.error(error.response ? error.response.body : error);
+  }
+});
+
+router.put("/goals/:id", async (req, res) => {
+  try {
+    res.status(200).json({ message: `Update goals ${req.params.id}` });
+  } catch (error) {
+    console.error(error.response ? error.response.body : error);
+  }
+});
+
+router.delete("/goals/:id", async (req, res) => {
+  try {
+    res.status(200).json({ message: `Delete goals ${req.params.id}` });
+  } catch (error) {
+    console.error(error.response ? error.response.body : error);
+  }
+});
 
 module.exports = router;
