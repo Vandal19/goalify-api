c72fd0045183c79063df40a9fcf80df57ca8b963
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-11-19 17:00:24 -0800

    Protecting the goals routes

--- routes/goalsRoute.js
+++ routes/goalsRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const {getGoals, setGoal, updateGoal, deleteGoal} = require("../controllers/goalController")
+const { protect } = require("../middleware/authMiddleware")
 
-router.get("/goals", getGoals);
-router.post("/goals", setGoal);
-router.put("/goals/:id", updateGoal);
-router.delete("/goals/:id", deleteGoal);
+router.get("/goals", (protect, getGoals));
+router.post("/goals", (protect, setGoal));
+router.put("/goals/:id", (protect, updateGoal));
+router.delete("/goals/:id", (protect, deleteGoal));
 
 module.exports = router;
